884d804dc6a1aae274e1fa9a8985479377f9c3d6
beanv <beanv@msoe.edu>
2025-09-03 20:54:05 -0500

    did ex1

---
+++ ex1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+     int x;
+     cout << "Enter a number" ;
+     cin >> x;
+     int result = pow(x, 3);
+     cout << "The cube of " << x << " is " << result << endl;
+
+}
